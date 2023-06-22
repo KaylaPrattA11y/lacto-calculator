189ca9dfcd27a1bcb92afd6fc91b12c2a075c6f2
kayla.pratt <kayla.pratt@fleetcor.com>
2023-06-22 12:37:50 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://kaylapratta11y.github.io',
+  base: '/lacto-calculator',
+});
